a5f6ae673918817135b244c3793a6d54f3ce1e72
kennyoungC <96569081+kennyoungC@users.noreply.github.com>
2022-04-21 20:42:49 +0300

    all tasks completed

--- src/Components/SingleBook.jsx
+++ src/Components/SingleBook.jsx
@@ -1,19 +1,31 @@
 import { Card, Button, Container, Row, Col } from "react-bootstrap"
+import { Component } from "react"
 // import ScifiBook from "./Data/book.json"
-const SingleBook = (props) => (
-  <Container>
-    <Row>
-      <Col>
-        <Card className="text-dark mb-3">
-          <Card.Img variant="top" src={props.book.img} />
-          <Card.Body>
-            <Card.Title>{props.book.title}</Card.Title>
+class SingleBook extends Component {
+  state = {
+    selected: false,
+  }
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Col>
+            <Card
+              className="text-dark mb-3"
+              onClick={() => this.setState({ selected: !this.state.selected })}
+              style={{ border: this.state.selected ? "2px solid red" : "none" }}
+            >
+              <Card.Img variant="top" src={this.props.book.img} />
+              <Card.Body>
+                <Card.Title>{this.props.book.title}</Card.Title>
 
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
-      </Col>
-    </Row>
-  </Container>
-)
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    )
+  }
+}
 export default SingleBook
