c0cae454b203c4580e06d486c16e60806c620987
Kenneth <obikenneth913@gmail.com>
2022-04-26 18:52:01 +0300

    commentarea moved to side col

--- src/Components/CommentArea.jsx
+++ src/Components/CommentArea.jsx
@@ -6,46 +6,47 @@ import ErrorComponent from "./ErrorComponent"
 class CommentArea extends Component {
   state = {
     comments: [],
-    isLoading: true,
+    isLoading: false,
     isError: false,
   }
-  componentDidMount = () => {
-    this.fetchComment()
-  }
-  fetchComment = async () => {
-    try {
-      const response = await fetch(
-        "https://striveschool-api.herokuapp.com/api/comments/" +
-          this.props.asin,
-        {
-          method: "GET",
-          headers: {
-            Authorization:
-              "Bearer  eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDgxN2M1ZWU3ODE4NzAwMTVjMjY3YTgiLCJpYXQiOjE2NTA2MDk2MjEsImV4cCI6MTY1MTgxOTIyMX0.JWoOs1eNwEoubIbDC32WsR1WR3K_FzvDvqISzIIztkI",
-          },
-        }
-      )
-      if (response.ok) {
-        const data = await response.json()
+  componentDidUpdate = async (prevProps) => {
+    if (prevProps.asin !== this.props.asin) {
+      this.setState({
+        isLoading: true,
+      })
+      try {
+        const response = await fetch(
+          "https://striveschool-api.herokuapp.com/api/comments/" +
+            this.props.asin,
+          {
+            headers: {
+              Authorization:
+                "Bearer  eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDgxN2M1ZWU3ODE4NzAwMTVjMjY3YTgiLCJpYXQiOjE2NTA2MDk2MjEsImV4cCI6MTY1MTgxOTIyMX0.JWoOs1eNwEoubIbDC32WsR1WR3K_FzvDvqISzIIztkI",
+            },
+          }
+        )
+        if (response.ok) {
+          const data = await response.json()
 
-        this.setState({
-          comments: data,
-          isLoading: false,
-          isError: false,
-        })
-      } else {
+          this.setState({
+            comments: data,
+            isLoading: false,
+            isError: false,
+          })
+        } else {
+          this.setState({
+            isLoading: false,
+            isError: true,
+          })
+          throw new Error("data not fetched")
+        }
+      } catch (error) {
+        console.log(error.message)
         this.setState({
           isLoading: false,
           isError: true,
         })
-        throw new Error("data not fetched")
       }
-    } catch (error) {
-      console.log(error.message)
-      this.setState({
-        isLoading: false,
-        isError: true,
-      })
     }
   }
   render() {
