d80256f7eca380de5d8bfd4d10c772e23a2ea399
Kenneth <obikenneth913@gmail.com>
2022-04-27 15:55:54 +0300

    SingleBook with hook

--- src/Components/SingleBook.jsx
+++ src/Components/SingleBook.jsx
@@ -1,39 +1,35 @@
 import { Card, Container, Row, Col } from "react-bootstrap"
 import { Component } from "react"
-class SingleBook extends Component {
+const SingleBook = (props) => {
   // state = {
   //   selected: false,
   // }
 
-  render() {
-    return (
-      <Container>
-        <Row>
-          <Col>
-            <Card
-              className="text-dark mb-3"
-              style={{
-                border:
-                  this.props.selectedBook === this.props.book.asin
-                    ? "2px solid red"
-                    : "none",
-              }}
-              onClick={() =>
-                this.props.changeSelectedBook(this.props.book.asin)
-              }
-            >
-              <Card.Img variant="top" src={this.props.book.img} />
-              <Card.Body>
-                <Card.Title>{this.props.book.title}</Card.Title>
-                {/* {this.state.selected && (
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <Card
+            className="text-dark mb-3"
+            style={{
+              border:
+                props.selectedBook === props.book.asin
+                  ? "2px solid red"
+                  : "none",
+            }}
+            onClick={() => props.changeSelectedBook(props.book.asin)}
+          >
+            <Card.Img variant="top" src={props.book.img} />
+            <Card.Body>
+              <Card.Title>{props.book.title}</Card.Title>
+              {/* {this.state.selected && (
                   <CommentArea asin={this.props.book.asin} />
                 )} */}
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-      </Container>
-    )
-  }
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  )
 }
 export default SingleBook
