a09f800fb44201f98dccaa09e422aaf684986b96
Kenneth <obikenneth913@gmail.com>
2022-04-27 14:23:41 +0300

    Booklist component with hooks

--- src/Components/BookList.jsx
+++ src/Components/BookList.jsx
@@ -1,55 +1,49 @@
 import SingleBook from "./SingleBook"
 import { Container, Col, Row, InputGroup, FormControl } from "react-bootstrap"
-import { Component } from "react"
+import { useState } from "react"
 import CommentArea from "./CommentArea"
-class BookList extends Component {
-  state = {
-    query: "",
-    selectedBook: null,
-  }
+const BookList = (props) => {
+  // state = {
+  //   query: "",
+  //   selectedBook: null,
+  // }
+  const [query, setQuery] = useState("")
+  const [selectedBook, setselectedBook] = useState(null)
 
-  render() {
-    return (
-      <Container>
-        <Row>
-          <Col>
-            <InputGroup className="mb-3">
-              <FormControl
-                placeholder="Search For Books"
-                value={this.state.query}
-                onChange={(e) => this.setState({ query: e.target.value })}
-              />
-            </InputGroup>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Row>
-              {this.props.book
-                .filter(
-                  (b) => b.title.toLowerCase().indexOf(this.state.query) !== -1
-                )
-                .map((b, i) => (
-                  <Col sm={6} key={i + 1}>
-                    <SingleBook
-                      book={b}
-                      selectedBook={this.state.selectedBook}
-                      changeSelectedBook={(asin) =>
-                        this.setState({
-                          selectedBook: asin,
-                        })
-                      }
-                    />
-                  </Col>
-                ))}
-            </Row>
-          </Col>
-          <Col md={4}>
-            <CommentArea asin={this.state.selectedBook} />
-          </Col>
-        </Row>
-      </Container>
-    )
-  }
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <InputGroup className="mb-3">
+            <FormControl
+              placeholder="Search For Books"
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
+            />
+          </InputGroup>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Row>
+            {props.book
+              .filter((b) => b.title.toLowerCase().indexOf(query) !== -1)
+              .map((b, i) => (
+                <Col sm={6} key={i + 1}>
+                  <SingleBook
+                    book={b}
+                    selectedBook={selectedBook}
+                    changeSelectedBook={(asin) => setselectedBook(asin)}
+                  />
+                </Col>
+              ))}
+          </Row>
+        </Col>
+        <Col md={4}>
+          <CommentArea asin={selectedBook} />
+        </Col>
+      </Row>
+    </Container>
+  )
 }
 export default BookList
